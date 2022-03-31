5eba30f0bcd46663155d49e8a4d4552dad810230
yryskul <kuttubaevayryskul@gmail.com>
2022-03-31 20:05:12 +0600

    add com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Table from "./component/Table/Table";
+import Table1 from "./component/Table1/Table1";
 
 function App() {
 
   return (
     <div className="App">
      <Table/>
+     
+     <Table1/>
     </div>
   );
 }
